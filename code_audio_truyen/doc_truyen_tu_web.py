e5dd998797eb989438401e7f2bb23eacd9e4a84a
huan-nv <ThaoQuynh!123>
2025-09-10 20:50:29 +0700

    u

--- code_audio_truyen/doc_truyen_tu_web.py
+++ code_audio_truyen/doc_truyen_tu_web.py
@@ -8,12 +8,12 @@ import time
 
 
 # tên truyện sẽ được cài đặt làm tên file
-TRUYEN_NAME = "da-tinh-ha-ngoc"
+TRUYEN_NAME = "trung-nu-dang-ho"
 # link truyện bắt đầu từ chương 1
 # trong url thay số chương bằng %chuong
 URL = f"https://truyenfull.vision/{TRUYEN_NAME}/chuong-%chuong"
 # số chương tối đa(chương cuối)
-END_CHAP = 10
+END_CHAP = 16
 # id thẻ chứa nội dung truyện
 ID_THE_NOI_DUNG = "chapter-c"
 # thư mục chứa nội dung truyện
